e7cb3a9c07eeab5a3c78ecad95a058f2ed3df968
thanhtung0212 <nguyenthanhtung@Nguyens-MacBook-Pro.local>
2020-03-05 12:28:36 +0700

    jquery

---
+++ assets/main.js
@@ -0,0 +1,35 @@
+;(function($) {
+
+    'use strict'
+
+    var spacer = function() {
+        $(window).on('load resize', function() {
+            var mode = 'desktop';
+
+            if ( matchMedia( 'only screen and (max-width: 991px)' ).matches )
+                mode = 'mobile';
+
+            if ( matchMedia( 'only screen and (max-width: 767px)' ).matches )
+                mode = 'smobile';
+
+            $('.nice-spacer').each(function(){
+                if ( mode == 'desktop' ) {
+                    $(this).attr('style', 'height:' + $(this).data('desktop') + 'px')
+                } else if ( mode == 'mobile' ) {
+                    $(this).attr('style', 'height:' + $(this).data('mobi') + 'px')
+                } else {
+                    $(this).attr('style', 'height:' + $(this).data('smobi') + 'px')
+                }
+            })
+
+        });
+    };
+
+
+    // Dom Ready
+    $(function() {
+        spacer();
+    });
+
+
+})(jQuery);
